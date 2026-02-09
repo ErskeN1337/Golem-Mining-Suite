8970f408dd34740bde2a934fe8c4cd6d2c6bfb6f
Ersken <ericnyman21@outlook.com>
2026-02-09 21:52:24 +0100

    fix: Make UpdateInfo.ReleaseUrl and ReleaseName nullable to fix CS9035 build errors

--- Golem Mining Suite/Services/UpdateInfo.cs
+++ Golem Mining Suite/Services/UpdateInfo.cs
@@ -5,9 +5,9 @@ namespace Golem_Mining_Suite
         public bool IsUpdateAvailable { get; set; }
         public required string CurrentVersion { get; set; }
         public required string LatestVersion { get; set; }
-        public required string ReleaseUrl { get; set; }
+        public string? ReleaseUrl { get; set; }
         public required string DownloadUrl { get; set; }
         public required string ReleaseNotes { get; set; }
-        public required string ReleaseName { get; set; }
+        public string? ReleaseName { get; set; }
     }
 }
